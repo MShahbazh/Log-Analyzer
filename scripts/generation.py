4a0f23e6ad8c406c8c32bbd280e182b9b81c3e0a
MShahbazh <msh23904t@gmail.com>
2026-05-22 02:57:07 +0500

    Generator Modified

--- scripts/generation.py
+++ scripts/generation.py
@@ -1,15 +1,51 @@
 import random 
 import sys 
-from datetime import date,datetime,timedelta
+from datetime import datetime,timedelta
 
-random_words=['Hello','Timestamp','Why','WHO','the','IS','hello world','happy nation','ip','']
+random_words=['Hello','Timestamp','Why','WHO','the','IS','hello world','happy nation','ip','RANDOM','WORDS','-','#','ERROR']
 methods=['GET','POST','DELETE','PUT','']
 routes=['/api/user','/api/login','/api/register','']
 status=['200','400','500','-','']
 units=['ms','s','']
+CRASH_PROB=0.2
+UNCONTROLLED_FORMAT=0.2
+
+def damage_string(text,type):
+    if random.random()<=CRASH_PROB:
+        if random.random()<=0.5:
+            t=text[:random.randint(0,len(text)-1)]
+            if t: 
+                text=t
+        if random.random()<=0.5:
+            if type=='json':
+                text=text.replace('{','#')
+            # elif type=='non':
+            #     text=text.replace(' ','(')
+        if random.random()<=0.5:
+            parts=text.split(' ')
+            p=parts
+            p.pop(random.randint(0,len(parts)-1)) 
+            if p:
+                parts=p 
+            text=''
+            for i in range(0,len(parts)):
+                text+=parts[i]
+    return text
+        
+        
+
+
+def generate_random_gibberish(x=1):
+    s=''
+    for i in range(0,x):
+        s+=random_words[random.randint(0,len(random_words)-1)]
+        if i!=x-1 and i!=0:
+            s+=' '
+    return s
 
 def generate_datetimes():
-    if random.random()<=0.2:
+    main_string=''
+    if random.random()<=UNCONTROLLED_FORMAT:
         format_sign=''
         space=''
         if random.choice([0,1])==0:
@@ -18,63 +54,63 @@ def generate_datetimes():
             format_sign='/'
         if random.choice([0,1])==0:
             space='T'
-        error_date=f"{random.randint(2000,2100)}+{format_sign}+{random.randint(-12,12)}+{format_sign}+{random.randint(-30,30)}+{space}+{format_sign}+{random.randint(-23,23)}+{format_sign}+{random.randint(-30,30)}+{format_sign}+{random.randint(-59,59)}+{format_sign}+{random.randint(-59,59)}+{format_sign}"
+        error_date=f"{random.randint(2000,2100)}{format_sign}{random.randint(-12,12)}{format_sign}{random.randint(-30,30)}{space}{random.randint(-23,23)}{format_sign}{random.randint(-30,30)}{format_sign}{random.randint(-59,59)}{format_sign}{random.randint(-59,59)}"
         if random.choice([0,1])==0:
             error_date+='Z'        
-        return error_date
-        
-    date1=datetime.now()
-    date2=datetime(2000,1,1,0,0,0,0)
-    diff=date1-date2
-    new_date=date1+timedelta(days=random.randint(0,diff.days))
-    unix_sec=new_date.timestamp()
-
-    if random.choice([0,1])==0:
-        f=''
-        if random.choice([0,1])==0:
-            f="%Y-%m-%d"
-        else:
-            f="%Y/%m/%d"
-        f+=" %H:%M:%S"
-        new_date=new_date.strftime(f)
-        
-        x=random.choice([0,1])
+        main_string=error_date
     else:
-        new_date=str(new_date)
-    x=random.choice([0,1,2])
-    x=0
-    if x==0: 
-        if random.choice([0,1]) == 0:
-            return new_date
-        else:
-            date3=new_date.split(' ')
-            if random.random()<=0:
-                print("ENRINER ")
-                if random.choice([0,1])==0:
-                    return f"{date3[0]}T{date3[1]}Z"
-                else:
-                    return f"{date3[0]}T{date3[1]}"
+        date1=datetime.now()
+        date2=datetime(2000,1,1,0,0,0,0)
+        diff=date1-date2
+        new_date=date1+timedelta(days=random.randint(0,diff.days))
+        unix_sec=new_date.timestamp()
+        if random.choice([0,1])==0:
+            f=''
+            if random.choice([0,1])==0:
+                f="%Y-%m-%d"
             else:
-                empty_spot=''
-                
+                f="%Y/%m/%d"
+            f+=" %H:%M:%S"
+            new_date=new_date.strftime(f)            
+        else:
+            new_date=str(new_date)
+       
+        x=random.choice([0,1])
+        
+        if x==0: 
+            if random.choice([0,1]) == 0:
+                main_string=new_date
+            else:
                 date3=new_date.split(' ')
-                e=[False,False]
-                if random.random()<=0.5: 
-                    empty_spot+=date3[0]
-                    e[0]=True
-                
-                if random.random()<=0.5: 
-                    empty_spot+=date3[1]
-                    e[1]=True
-                
-                if e[0] and e[1]:
+                if random.random()<=0:
                     if random.choice([0,1])==0:
-                        empty_spot=f"{date3[0]}T{date3[1]}Z"
+                        main_string= f"{date3[0]}T{date3[1]}Z"
                     else:
-                        empty_spot=f"{date3[0]}T{date3[1]}"
-                return empty_spot         
-    elif x==1:
-        return str(int(unix_sec))
+                        main_string= f"{date3[0]}T{date3[1]}"
+                else:
+                    empty_spot=''
+                    date3=new_date.split(' ')                    
+                    e=[False,False]
+                    if random.random()<=0.5: 
+                        empty_spot+=date3[0]
+                        e[0]=True
+                    if random.random()<=0.5: 
+                        empty_spot+=date3[1]
+                        e[1]=True                
+                    if e[0] and e[1]:
+                        if random.choice([0,1])==0:
+                            empty_spot=f"{date3[0]}T{date3[1]}Z"
+                        else:
+                            empty_spot=f"{date3[0]}T{date3[1]}"
+                    if not e[0] and not e[1]:
+                        empty_spot=new_date
+                    main_string=empty_spot                      
+        elif x==1:
+            main_string=str(int(unix_sec))
+
+        if random.random()<=CRASH_PROB:
+            main_string=damage_string(main_string,'json')
+    return main_string
        
 def generate_ip():
     ip=[0]*4
@@ -85,7 +121,10 @@ def generate_ip():
         ip[0]=random.randint(0,10)
     else:
         ip[0]=random.randint(0,255)
-    return f"{ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}"
+    text=f"{ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}"
+    if random.random()<=0.05:
+        text=damage_string(text)
+    return text
     
 def json_generator():
         a=generate_datetimes()
@@ -93,7 +132,9 @@ def json_generator():
         if a=='':
            s+=''
         else:
-            s+=a
+            if a is None: 
+                print(f"NOEN!!! {a}")
+            s+="hello"
 
         a=generate_ip()
         s+='","Ip":"'
@@ -131,7 +172,54 @@ def json_generator():
         else:
             s+=a
         s+='"}'
+        
+
+        if random.random()<=CRASH_PROB:
+            s=damage_string(s,'json')
         return s
+
+def non_json_generator():
+        s=''
+        a=generate_datetimes()
+        if a=='':
+           s+=''
+        else:
+            s+=a
+        a=generate_ip()
+        s+=' '
+        if a=='':
+           s+=''
+        else:
+            s+=a
+        s+=' '
+        a=methods[random.randint(0,len(methods)-1)]
+        if a=='':
+           s+=''
+        else:
+            s+=a
+        s+=' '
+        a=routes[random.randint(0,len(routes)-1)]
+        if a=='':
+           s+=''
+        else:
+            s+=a
+        s+=' '
+        a=status[random.randint(0,len(status)-1)]
+        if a=='':
+           s+=''
+        else:
+            s+=a
+        s+=' '
+        a=units[random.randint(0,len(units)-1)]
+        s+=str(random.randint(0,1000))
+        if a=='':
+           s+=''
+        else:
+            s+=a
+        if random.random()<=CRASH_PROB:
+            s=damage_string(s,'non')
+        return s
+
 
 fd=open('test_logs/file.log','w+')
 nums=10
@@ -141,6 +229,20 @@ else:
     nums=int(sys.argv[1])
 
 for _ in range(0,nums):
-    k=json_generator()
+    k=''
+    x=random.random()
+    if x>=0.5:
+        k=json_generator()
+    elif x>=0.3:
+        k=non_json_generator()
+    elif x>=0.05:
+        k=generate_random_gibberish(random.randint(0,len(random_words)-1))
+    elif x>=0:
+        k=''
     k+='\n'
     fd.write(k)
+
+    
+    
+        
+
